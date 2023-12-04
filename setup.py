28bcdc28915f1f3f060f8ba7a1351ad2ec510773
zhiyuan hu <levi.huzhiyuan@gmail.com>
2023-12-05 00:27:26 +0100

    update to adjust hydra 1.3

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup
 
 install_requires = [
     'torch>=1.5.1',
-    'gym>=0.17.2',
+    'gym==0.23.1',
     'numpy>=1.10.4',
     'pillow',
     "mujoco-py>=2.1, <2.2",
     "scipy",
+    'hydra-core',
     # "hydra-core==1.0.6",
     # "omegaconf==2.0.6",
     "Cython<3"
